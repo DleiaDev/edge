9f78f6dc27e7f3ca6def103ad00bf69bf9f2a7ca
DleiaDev <markowebdeveloper@gmail.com>
2024-05-01 01:01:55 +0200

    feat: add default layout and a page

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <aside>Sidebar</aside>
+    <div>
+      <header>Header</header>
+      <main>
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="page page--dashboard">Hello from page</div>
+</template>
+
+<script setup lang="ts"></script>
